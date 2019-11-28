1bba2694bd2fd9965cf25d3707f3edcc7dd2542c
bob-crutchley <bob.crutchley96@gmail.com>
2019-11-28 20:47:53 +0000

    changed domain name suffix

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ module "website" {
   resource_group = azurerm_resource_group.default
   virtual_network = azurerm_virtual_network.default
   subnet_cidr = "10.0.1.0/24"
-  domain_name_suffix = "bob-crutchley"
+  domain_name_suffix = "postcard"
 }
 
 module "api" {
@@ -25,7 +25,7 @@ module "api" {
   resource_group = azurerm_resource_group.default
   virtual_network = azurerm_virtual_network.default
   subnet_cidr = "10.0.2.0/24"
-  domain_name_suffix = "bob-crutchley"
+  domain_name_suffix = "postcard"
 }
 
 module "database" {
