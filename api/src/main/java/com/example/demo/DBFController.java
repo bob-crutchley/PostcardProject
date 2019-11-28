18e8302d82d72bafca2999943a71247e5708ee76
bob-crutchley <bob.crutchley96@gmail.com>
2019-11-28 18:26:04 +0000

    removed api context

--- api/src/main/java/com/example/demo/DBFController.java
+++ api/src/main/java/com/example/demo/DBFController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController()
 public class DBFController {
 	@Autowired
 	private PostcardRepo repo;
